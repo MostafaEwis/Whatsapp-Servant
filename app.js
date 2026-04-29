1aee74fde443df20f9ea97bfcf88efd7a33da2cc
Mostafa Ewis <64862002+MostafaEwis@users.noreply.github.com>
2026-04-29 07:38:05 +0300

    now whatsapp returns the reverse of your message, whatever it is

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
-const verifyToken = 'ewis';
+const verifyToken = process.env.VerifyToken;
+const wpmToken = process.env.wpmToken;
+const phoneNumber = process.env.phoneNumber;
+const url = process.env.wpmURL;
 
 app.get('/', (req, res) => {
   const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
@@ -18,10 +21,30 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(`\n\nWebhook received ${timestamp}\n`);
-  console.log(JSON.stringify(req.body, null, 2));
-  res.status(200).end();
+	const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+	const msgBody = JSON.stringify(req.body, null, 2);
+	const msg = msgBody.entry[0].changes[0].value.messages[0].text.body;
+	try{
+		const res = await fetch(url, {
+			method: 'POST',
+			headers:{
+				"Content-Type": "application/json",
+				"Authorization": wpmToken
+			},
+			body: JSON.stringify({
+				"messaging_product":"whatsapp",
+				"to": phoneNumber,
+				"type":"text",
+				"text":{
+					"body": msg.split('').reverse().join('')
+				}
+			})
+		});
+		const data = await res.json();
+	}catch(e){
+		console.log(e);
+	}
+	res.status(200).end();
 });
 app.get('/test', (req, res) => {
 	res.json({"mostafa": "ewis"});
